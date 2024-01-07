fe733a08735e6449dd7e2dd2bf92a20b211bb08b
Raushan1234567 <raushankumar241099@gmail.com>
2024-01-07 14:43:50 +0530

    updated

--- PlatformCommon/src/main/java/com/platform/controller/AdminController.java
+++ PlatformCommon/src/main/java/com/platform/controller/AdminController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.platform.entity.Admin;
 import com.platform.repository.AdminRepository;
 import com.platform.serviceinterface.AdminServiceInterface;
 
+@CrossOrigin(originPatterns = "*")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
--- PlatformCommon/src/main/java/com/platform/controller/StudentController.java
+++ PlatformCommon/src/main/java/com/platform/controller/StudentController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.platform.entity.Course;
 import com.platform.entity.Student;
 import com.platform.serviceinterface.StudentServiceInterface;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/students")
 public class StudentController {
